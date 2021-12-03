623edc804845a2f412f51e2a9de6e1b5a9306f0a
joshuapa <josh.paulin@ukg.com>
2021-12-03 12:17:46 -0500

    Reading a single file

--- wheniwork/app.py
+++ wheniwork/app.py
@@ -1,2 +1,17 @@
+from csv import reader
+from urllib.request import urlopen
+from contextlib import closing
+base_path = 'https://public.wiwdata.com/engineering-challenge/data/'
+file_name = 'a'
+
+
 def run():
-    print('Hello World')
+    # Read files
+    with closing(urlopen('https://public.wiwdata.com/engineering-challenge/data/a.csv')) as file:
+        csv_file = reader(file.read().decode('utf-8').splitlines())
+        data = list(csv_file)
+        print(data)
+
+    # Map Reduce
+    # Output CSV
+    return 0
