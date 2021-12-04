97a535c38d5ff587536079cb538473d2cafc8167
joshuapa <josh.paulin@ukg.com>
2021-12-04 16:22:06 -0500

    Write to file

--- wheniwork/__main__.py
+++ wheniwork/__main__.py
@@ -1,8 +1,10 @@
 from sys import argv
 from app import run
 
-DEFAULT_BASE_PATH = 'https://public.wiwdata.com/engineering-challenge/data/'
+DEFAULT_INPUT_PATH = 'https://public.wiwdata.com/engineering-challenge/data/'
+DEFAULT_OUTPUT_FILE = './output.csv'
 
 if __name__ == '__main__':
-    path = argv[1] if len(argv) == 2 else DEFAULT_BASE_PATH
-    run(path)
+    input_path = argv[1] if len(argv) >= 2 else DEFAULT_INPUT_PATH
+    output_file = argv[2] if len(argv) >= 3 else DEFAULT_OUTPUT_FILE
+    run(input_path, output_file)
--- wheniwork/app.py
+++ wheniwork/app.py
@@ -1,24 +1,25 @@
 from csv import reader
+from csv import writer
 from urllib.request import urlopen
 from contextlib import closing
 from common.constants import InputCSV
 from string import ascii_lowercase
 
 
-def run(base_path):
+def run(base_input_path, output_file):
     combined_visits = {}
     all_paths = set()
 
     for c in ascii_lowercase:
 
         # Read files
-        visits = read_file(f'{base_path}{c}.csv')
+        visits = read_file(f'{base_input_path}{c}.csv')
 
         for visit in visits:
             all_paths.add(visit.path)
             add_visit(combined_visits, visit.user, visit.path, visit.length)
 
-    return 0
+    write_file(list(all_paths), combined_visits, output_file)
 
 
 def read_file(url):
@@ -30,6 +31,19 @@ def read_file(url):
         data.pop(0)
 
         return [PathVisit(row[InputCSV.USER_ID], row[InputCSV.PATH], int(row[InputCSV.LENGTH])) for row in data]
+
+
+def write_file(paths, combined_visits, output_file):
+    with open(f'{output_file}', 'w') as csv_file:
+        csv_writer = writer(csv_file)
+
+        csv_writer.writerow(['user_id', *paths])
+        for user_id, visits in combined_visits.items():
+            row = [user_id]
+            for path in paths:
+                row.append(visits.get(path, 0))
+            csv_writer.writerow(row)
+
 
 
 def add_visit(final_data, user, path, length):
