4f6a9dfc7edf7056e56cddc813d0bed1ab62316a
joshuapa <josh.paulin@ukg.com>
2021-12-05 16:03:14 -0500

    Documentation

---
+++ README.md
@@ -0,0 +1,17 @@
+# When I Work: Web Traffic Transform
+
+## Instalation
+This program uses vanilla Python 3. No explicit instalation is required other than Python 3 itself.
+
+## Running
+1. Clone the repository
+2. From the project root folder run `python -m webtraffictransformation`
+
+### Command Line Arguments
+* `-i` (`--input`): Set the base URL for the raw traffic files; must include the trailing `/`
+  * Default value: https://public.wiwdata.com/engineering-challenge/data/
+* `-o` (`--output`): Path and file name for the output .csv
+  * Default value: `./output.csv`
+
+### Running tests
+From the root directory run `python -m unittest discover webtraffictransformation/tests/` 
--- webtraffictransformation/__main__.py
+++ webtraffictransformation/__main__.py
@@ -6,9 +6,9 @@ DEFAULT_INPUT_PATH = 'https://public.wiwdata.com/engineering-challenge/data/'
 DEFAULT_OUTPUT_FILE = './output.csv'
 
 parser = ArgumentParser()
-parser.add_argument('--input', '-i', help='The input path for the files to parse. Must include the final "/"',
+parser.add_argument('--input', '-i', help='The input path for the files to parse. Must include the trailing "/"',
                     default=DEFAULT_INPUT_PATH)
-parser.add_argument('--output', '-o', help='Path and file name for the output csv', default=DEFAULT_OUTPUT_FILE)
+parser.add_argument('--output', '-o', help='Path and file name for the output .csv', default=DEFAULT_OUTPUT_FILE)
 
 if __name__ == '__main__':
     args = parser.parse_args()
