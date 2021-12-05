01d067f84b92b79ae4e6f827212850a64271fcd3
joshuapa <josh.paulin@ukg.com>
2021-12-05 13:40:56 -0500

    Better argument parsing for input and output

--- wheniwork/__main__.py
+++ wheniwork/__main__.py
@@ -1,10 +1,15 @@
-from sys import argv
+from argparse import ArgumentParser
+
 from app import run
 
 DEFAULT_INPUT_PATH = 'https://public.wiwdata.com/engineering-challenge/data/'
 DEFAULT_OUTPUT_FILE = './output.csv'
 
+parser = ArgumentParser()
+parser.add_argument('--input', '-i', help='The input path for the files to parse. Must include the final "/"',
+                    default=DEFAULT_INPUT_PATH)
+parser.add_argument('--output', '-o', help='Path and file name for the output csv', default=DEFAULT_OUTPUT_FILE)
+
 if __name__ == '__main__':
-    input_path = argv[1] if len(argv) >= 2 else DEFAULT_INPUT_PATH
-    output_file = argv[2] if len(argv) >= 3 else DEFAULT_OUTPUT_FILE
-    run(input_path, output_file)
+    args = parser.parse_args()
+    run(args.input, args.output)
